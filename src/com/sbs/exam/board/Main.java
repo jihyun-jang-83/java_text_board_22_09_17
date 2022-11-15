9d596f79486e8939541fec1ed8cceb52f2f38bd4
jihyun-jang-83 <wini_jihyun@hanmail.net>
2022-11-15 18:59:01 +0900

    C:/Program Files/Git/usr/article/detail 명령어로 최근 게시물 보기

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -7,6 +7,10 @@ public class Main {
 
     Scanner sc = new Scanner(System.in);
     int articleLastId = 0;
+    Article lastArticle = null;
+
+    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 프로그램 시작 ==");
 
     while(true) {
       System.out.printf("명령 ) ");
@@ -15,6 +19,19 @@ public class Main {
       if(cmd.equals("exit")) {
         break;
       }
+
+      else if(cmd.equals("/usr/article/detail")) {
+        if ( lastArticle == null ) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+        Article article = lastArticle;
+        System.out.println("== 게시물 상세내용 ==");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.title);
+        System.out.printf("내용 : %s\n", article.body);
+      }
+
       else if(cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 등록 ==");
         System.out.printf("제목 : ");
@@ -23,10 +40,8 @@ public class Main {
         String body = sc.nextLine();
         int id = ++articleLastId;
 
-        Article article = new Article();
-        article.id = id;
-        article.title = title;
-        article.body = body;
+        Article article = new Article(id, title, body);
+        lastArticle = article;
 
         System.out.println("생성된 게시물 객체 : " + article);
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
@@ -35,6 +50,10 @@ public class Main {
         System.out.printf("입력된 명령어 : %s\n", cmd);
       }
     }
+
+    System.out.println("== 프로그램 종료 ==");
+
+    sc.close();
 
     /*
     int num = sc.nextInt();
@@ -52,8 +71,13 @@ class Article {
   String title;
   String body;
 
+  Article(int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
   @Override
   public String toString() {
-    return String.format("\nid : %d\ntitle : %s\nbody : %s\n", id, title, body);
+    return String.format("{id: %d, title : \"%s\", body : \"%s\"}", id, title, body);
   }
 }
