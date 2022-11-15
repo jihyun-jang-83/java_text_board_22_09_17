30103f19ce9162f4385eefa33e8c7d7730c4037c
jihyun-jang-83 <wini_jihyun@hanmail.net>
2022-11-15 11:24:44 +0900

    새 게시물 입력받기 기능 구현 시작

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -7,23 +7,34 @@ public class Main {
 
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("== 게시판 v 0.1 ==");
-    System.out.println("== 프로그램 시작 ==");
     while(true) {
       System.out.printf("명령 ) ");
       String cmd = sc.nextLine();
+
       if(cmd.equals("exit")) {
         break;
       }
-      System.out.printf("입력된 명령어 : %s\n", cmd);
+      else if(cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 등록 ==");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+        int id = 1;
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+        }
+      else {
+        System.out.printf("입력된 명령어 : %s\n", cmd);
+      }
     }
 
     /*
     int num = sc.nextInt();
     System.out.printf("num = %d\n", num);
     */
-
+    /*
     sc.close();
     System.out.println("== 프로그램 종료 ==");
+    */
   }
 }
