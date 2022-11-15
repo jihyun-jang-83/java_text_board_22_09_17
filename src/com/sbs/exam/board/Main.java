609cf3fdf311d2408bbfe4d7626210b8529b8d4f
jihyun-jang-83 <wini_jihyun@hanmail.net>
2022-11-15 10:54:13 +0900

    exit 명령이 나올 때까지 계속 입력받기

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
     Scanner sc = new Scanner(System.in);
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.printf("명령 ) ");
-    String cmd = sc.nextLine();
-    System.out.println("입력된 명령어 : " + cmd);
+    while(true) {
+      System.out.printf("명령 ) ");
+      String cmd = sc.nextLine();
+      if(cmd.equals("exit")) {
+        break;
+      }
+      System.out.printf("입력된 명령어 : %s\n", cmd);
+    }
 
     /*
     int num = sc.nextInt();
