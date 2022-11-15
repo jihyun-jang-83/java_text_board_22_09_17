a36e01e0bda7384d142d4af73e0145365cf8ffbd
jihyun-jang-83 <wini_jihyun@hanmail.net>
2022-11-15 10:39:53 +0900

    명령어 입력받기

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -1,7 +1,25 @@
 package com.sbs.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    Scanner scanner = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 프로그램 시작 ==");
+    System.out.printf("명령 ) ");
+    String cmd = sc.nextLine();
+    System.out.println("입력된 명령어 : " + cmd);
+
+    /*
+    int num = sc.nextInt();
+    System.out.printf("num = %d\n", num);
+    */
+
+    sc.close();
+    System.out.println("== 프로그램 종료 ==");
   }
 }
