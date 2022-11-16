4289f2ecc0a78caec640f503d958f1e093db4353
jihyun-jang-83 <wini_jihyun@hanmail.net>
2022-11-16 17:32:22 +0900

    lastArticle 변수의 필요성을 제거

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -13,7 +13,6 @@ public class Main {
 
     Scanner sc = new Scanner(System.in);
     int articleLastId = 0;
-    Article lastArticle = null;
 
     ArrayList<Article> articles = new ArrayList<Article>();
 
@@ -48,11 +47,11 @@ public class Main {
       }
 
       else if(cmd.equals("/usr/article/detail")) {
-        if ( lastArticle == null ) {
+        if ( articles.isEmpty() ) {
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
-        Article article = lastArticle;
+        Article article = articles.get(articles.size() - 1 );
         System.out.println("== 게시물 상세내용 ==");
         System.out.printf("번호 : %d\n", article.id);
         System.out.printf("제목 : %s\n", article.title);
@@ -68,7 +67,6 @@ public class Main {
         int id = ++articleLastId;
 
         Article article = new Article(id, title, body);
-        lastArticle = article;
 
         articles.add(article);
 
